c0872985bcbe135f5fdee6ddacceca7b8d78e7b1
rajeshp7 <rajesh.p@gramener.com>
2020-10-07 15:20:52 +0530

    setup.cfg change

--- src/gramenertest/__init__.py
+++ src/gramenertest/__init__.py
@@ -4,7 +4,7 @@ from pkg_resources import get_distribution, DistributionNotFound
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
-    __version__ = get_distribution(dist_name).version
+    __version__ = 1.0.0  # get_distribution(dist_name).version
 except DistributionNotFound:
     __version__ = 'unknown'
 finally:
